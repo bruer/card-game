e72088571456338e4701326ebb6e129a6bd0f1ad
bruer <f.bruer@gmail.com>
2021-09-30 11:28:15 +0200

    test commit

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import { clubs, diamonds, hearts, spades } from "./deck.js";
+import { clubs, diamonds, hearts, spades } from './deck.js';
 
-const dealCardsButton = document.querySelector("button");
-dealCardsButton.addEventListener("click", setupGame);
+const dealCardsButton = document.querySelector('button');
+dealCardsButton.addEventListener('click', setupGame);
 
 function setupGame() {
   const handMaxSize = 5;
   const twoPair = 2;
   const { hand, pairs } = deal(handMaxSize, twoPair);
 
-  const oldContainer = document.querySelector("#container");
+  const oldContainer = document.querySelector('#container');
   const handContainer = createHandContainer(hand);
   const result = createResult(pairs, twoPair);
   const newContainer = createContainer([
-    pairs.length < 2 ? dealCardsButton : "",
+    pairs.length < 2 ? dealCardsButton : '',
     handContainer,
     result,
   ]);
@@ -54,20 +54,20 @@ function findPair(pairs, hand, newCard) {
 }
 
 function createContainer(content) {
-  const container = document.createElement("div");
-  container.setAttribute("id", "container");
+  const container = document.createElement('div');
+  container.setAttribute('id', 'container');
   container.append(...content);
 
   return container;
 }
 
 function createHandContainer(cards) {
-  const handContainer = document.createElement("div");
-  handContainer.setAttribute("id", "hand-container");
+  const handContainer = document.createElement('div');
+  handContainer.setAttribute('id', 'hand-container');
 
   const listOfCards = createListOfCards(cards);
-  const info = document.createElement("strong");
-  info.innerText = "Your hand";
+  const info = document.createElement('strong');
+  info.innerText = 'Your hand';
 
   handContainer.append(info, listOfCards);
 
@@ -75,57 +75,57 @@ function createHandContainer(cards) {
 }
 
 function createResult(pairs, twoPair) {
-  const resultContainer = document.createElement("div");
-  resultContainer.setAttribute("id", "result-container");
+  const resultContainer = document.createElement('div');
+  resultContainer.setAttribute('id', 'result-container');
 
-  const result = document.createElement("div");
-  result.setAttribute("id", "result");
+  const result = document.createElement('div');
+  result.setAttribute('id', 'result');
 
-  const resultText = document.createElement("p");
+  const resultText = document.createElement('p');
 
   result.append(resultText);
   resultContainer.append(result);
 
   if (pairs.length < twoPair) {
-    resultText.innerText = "You lose!";
+    resultText.innerText = 'You lose!';
   }
   if (pairs.length >= twoPair) {
-    resultText.innerText = "You got a two pair!";
+    resultText.innerText = 'You got a two pair!';
 
     const listOfPairs = createListOfPairs(pairs);
     result.append(listOfPairs);
 
-    const continueButton = createButton("", "Play again");
+    const continueButton = createButton('', 'Play again');
     resultContainer.append(continueButton);
   }
   return resultContainer;
 }
 
 function createButton(id, content) {
-  const button = document.createElement("button");
-  button.setAttribute("id", id);
-  button.addEventListener("click", setupGame);
+  const button = document.createElement('button');
+  button.setAttribute('id', id);
+  button.addEventListener('click', setupGame);
   button.innerText = content;
 
   return button;
 }
 
 function createListOfCards(cards) {
-  const list = document.createElement("ul");
+  const list = document.createElement('ul');
 
   cards.forEach((card) => {
-    const item = document.createElement("li");
-    item.innerText = card.value + " of " + card.suit;
+    const item = document.createElement('li');
+    item.innerText = card.value + ' of ' + card.suit;
     list.append(item);
   });
   return list;
 }
 
 function createListOfPairs(pairs) {
-  const list = document.createElement("ol");
+  const list = document.createElement('ol');
 
   pairs.forEach((pair) => {
-    const item = document.createElement("li");
+    const item = document.createElement('li');
     item.innerText = `${pair[0].value} of ${pair[0].suit} and ${pair[1].value} of ${pair[1].suit}`;
     list.append(item);
   });
